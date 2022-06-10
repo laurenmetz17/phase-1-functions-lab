0d894614fde9ce1f100ef75c1741bb86f3f7769e
Lauren Metz <Laurenmetz15@gmail.com>
2022-06-10 19:30:30 -0400

    commit tree 44b8fd951f4fa00c588dc57c18c99e428e9b4c12

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const headquarters = 42;
+const blockFeet = 264;
+
+function distanceFromHqInBlocks(blockNumber) {
+    return Math.abs(blockNumber - headquarters); 
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * blockFeet;
+}
+
+function distanceTravelledInFeet(startBlock,endBlock) {
+    return Math.abs(startBlock - endBlock) * blockFeet;
+}
+
+function calculatesFarePrice(startBlock,endBlock) {
+    let distance = distanceTravelledInFeet(startBlock,endBlock);
+    switch(true) {
+        case distance <= 400:
+            return 0;
+        case distance > 400 && distance <= 2000:
+            return ((distance - 400) * 2)/100;
+        case distance > 2000 && distance <= 2500:
+            return 25;
+        case distance > 2500:
+            return 'cannot travel that far'
+    }
+}
